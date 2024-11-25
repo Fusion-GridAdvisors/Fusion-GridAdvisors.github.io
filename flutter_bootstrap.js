0d0eaa0e9ee6bb9c7299c8aa9a211a5402925bfb
Cristian Paraschiv <paraschiv.cristian93@outlook.com>
2024-11-25 02:48:03 +0000

    Navigation buttons! Footer on all pages! Responsive services grid!

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2646883631"
+    serviceWorkerVersion: "1661887094"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "89c826cbd4a9feeccc76e99f6aba91c3
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "b18f98079848458cec8da0dfb4a5893e",
+"flutter_bootstrap.js": "3fefd5b16b551681967744e41d486a23",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e3bd36ef6be6715d645257441ea20ab9",
 "/": "e3bd36ef6be6715d645257441ea20ab9",
-"main.dart.js": "dabcfe062f746ac634917efeb21fb1a0",
+"main.dart.js": "6bd7c094c84b71b050e758e38a6aaa3b",
 "manifest.json": "6969cac7faf192177de69469bf8be316",
 "version.json": "5bb900e8173795b7b15d53f93b55a72e"};
 // The application shell files that are downloaded before a service worker can
